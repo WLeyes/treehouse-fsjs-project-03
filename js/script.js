9c900b913fb41c89236bc0df77769dea345354c9
Warren Leyes <warren.leyes@gmail.com>
2018-09-01 19:34:08 -0700

    set focus on first input closes #3 Set form vars

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
-console.log('Connected to script.js');
-// $('.container').prepend('<div>Test</div>');
+$(document).ready(function () {
+  console.log('Connected to script.js');
+  // set focus to first input
+  $('form input:text').first().focus();
+  // hide all elements that need to be hidden
+});
+
+const formVariables = () => {
+
+  $basic = {
+    name: $('#name'),
+    email: $('#email'),
+    title: $('#title'),
+    titleOther: $('#other-title')
+  },
+
+  $shirt = {
+    size: $('#size'),
+    design: $('#design'),
+    color: $('#color')
+  },
+
+  $activities = {
+    check: $(':checked')
+  }
+
+  $payment = {
+    creditCard: $('#cc-num'),
+    zip: $('#zip'),
+    cvv: $('#cvv'),
+    month: $('#exp-month'),
+    year: $('#year')
+  }
+
+  $form = { 
+    submit: $('button[type="submit"]')
+  }
+}
