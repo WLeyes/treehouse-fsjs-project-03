e7cb7f62bc61979fcbcb932aefb388e2db9ca758
Warren Leyes <warren.leyes@gmail.com>
2018-09-03 00:35:39 -0700

    fixed basic info validation

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@
 
 const $variables = () => {
   $container = $('.container');
+  $fieldset = {
+    shirt: $('fieldset:nth-of-type(2)'),
+    activities: $('fieldset:nth-of-type(3)'),
+    payment: $('fieldset:nth-of-type(4)')
+  }
 
   $basicInfo = {
     name: $('#name'),
@@ -30,7 +35,7 @@ const $variables = () => {
   };
 
   $submit = $('button[type="submit"]');
-
+  $error = '.error';
 }
 // call variables
 $variables();
@@ -44,45 +49,54 @@ $(document).ready(function () {
 
   // hide all elements that need to be hidden
   $basicInfo.titleOther.hide();
-  $('fieldset').next().hide();
-  // $('fieldset').next().next().hide();
-  // Start with submit button disabled and greyed out
-  // $submit.attr('disabled', true)
-  //   .css({backgroundColor: 'lightgrey', color: 'darkgrey'});
+  $($fieldset.shirt).hide();
+  $($fieldset.activities).hide();
+  $($fieldset.payment).hide();
+  $($submit).hide();
 });
 
-const Validate = (element) => {
-  switch(element.attr('id')){
-    case 'name':
-      if(element.val().length > 0) {
-        element.css({border: '3px solid green', backgroundColor: 'lightgreen'});
-      } else {
-        element.css({border: '3px solid red', backgroundColor: 'pink'}).attr({placeholder: 'Required'});
-      }
-     break;
-    case 'mail':
-      // https://stackoverflow.com/questions/2507030/email-validation-using-jquery
-      const regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-      console.log(regex.test(element.val()));
-      if(regex.test(element.val()) === true) {
-        element.css({border: '3px solid green', backgroundColor: 'lightgreen'});
-      } else {
-        element.css({border: '3px solid red', backgroundColor: 'pink'}).attr({placeholder: 'Required'})
-      }
-  }
-}
 
 const Info = () => {
-  $('input[type="text"]').on('focusout input', event => {
+  // Set my validation tracker to false
+  $('input').attr({'data-valid': 'false'})
+
+  // check that name input is not empty
+  $('input[type="text"]').on('focusout input' , function(event) {
     event.preventDefault();
-    let name = $(event.target);
-    Validate(name);
+    if($(this).val().length > 0) {
+      $(this).css({border: '3px solid green', backgroundColor: 'lightgreen'}).attr({'data-valid': 'true'});
+    } else {
+      $(this).css({border: '3px solid red', backgroundColor: 'pink'}).attr({placeholder: 'Required'}).attr({'data-valid': 'false'});
+    }
   });
-  $('input[type="email"]').on('focusout input', event => {
-    event.preventDefault();
-    let email = $(event.target);
-    Validate(email);
+  
+  // check and validate email
+  $('input[type="email"]').on('focusout input' , function(event) {
+  event.preventDefault();
+  // check if email is valid
+  if($('input[type="email"]').val() !== '') {
+    // https://stackoverflow.com/questions/2507030/email-validation-using-jquery
+    const regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      if(regex.test($(this).val()) === true) {
+        if($('.error')){
+          $('.error').remove();
+        }
+        $(this).css({border: '3px solid green', backgroundColor: 'lightgreen'}).attr({'data-valid': 'true'});
+      } else {
+        $(this).css({border: '3px solid red', backgroundColor: 'pink'}).attr({placeholder: 'Required'}).attr({'data-valid': 'false'});
+        if($($error)){
+          $($error).remove();
+        }
+        $($fieldset.shirt).append('<div class="error">Please add valid email</div>')
+      }
+    }
   });
+  $('input').on('input' , function(event) {
+    if( $('input[type="text"]').attr('data-valid') === 'true' && $('input[type="email"]').attr('data-valid') === 'true'){
+      $fieldset.shirt.fadeIn(3000);
+    }
+  });
+
 }
 
 const Shirt = () => {
